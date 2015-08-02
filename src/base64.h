f5f369ab72759a6581efb58b45545aaefe8afe5d
Alexander Færøy <ahf@0x90.dk>
2015-08-02 12:29:58 +0200

    Add missing header.

--- src/base64.h
+++ src/base64.h
@@ -5,6 +5,8 @@
 #ifndef GUARD_BASE64_H
 #define GUARD_BASE64_H 1
 
+#include <stdbool.h>
+
 void base64_init(void);
 bool base64_encode(unsigned char *input, size_t input_size, unsigned char **output, size_t *output_size);
 bool base64_decode(unsigned char *input, size_t input_size, unsigned char **output, size_t *output_size);
